890ef67ee86036c9e60cef0b96abf657015c4331
Christian Paul <christian.paul@kiprotect.com>
2019-08-02 12:01:38 +0200

    CI build for Styleguidist

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,5 +1,7 @@
 // This is the configuration file for react-styleguidist
 module.exports = {
     components: 'src/components/**/*.{jsx,tsx}',
+    styleguideDir: 'build/styleguide',
+    title: 'Algoneer Style Guide',
     webpackConfig: require('./webpack-web.config.js'),
 };
