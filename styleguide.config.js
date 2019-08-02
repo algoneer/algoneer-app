4e28c9b356f4369edf4738bbef6aaecd47aa6a18
Christian Paul <christian.paul@kiprotect.com>
2019-08-02 10:21:35 +0200

    Upgrade to webpack 4, remove UglyJS, add react-styleguidist

---
+++ styleguide.config.js
@@ -0,0 +1,5 @@
+// This is the configuration file for react-styleguidist
+module.exports = {
+    components: 'src/components/**/*.{jsx,tsx}',
+    webpackConfig: require('./webpack-web.config.js'),
+};
