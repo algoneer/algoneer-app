424791cbf45ed7c9b91c3e2f8f551f9422c10416
Christian Paul <christian.paul@kiprotect.com>
2019-08-20 16:45:06 +0200

    Get the UserDropdown working

--- src/apps/main/header/user-dropdown.jsx
+++ src/apps/main/header/user-dropdown.jsx
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 import { withActions } from '7s/components';
 
 import placeholder from './avatar-placeholder.png';
 import './user-dropdown.scss';
 
+let id = 0;
+function uniqueId() {
+    id++;
+    return `id-${id}`;
+}
+
 const UserDropdown = ({user}) => {
-    console.log(user);
+    const [open, setOpen] = useState(true);
+    const [toggleId] = useState(uniqueId());
+    const [dropdownId] = useState(uniqueId());
     return (
-        <div>
-            <img
-                className="user-dropdown__image"
-                alt="Profile and settings menu"
-                src={placeholder}
-            />
+        <div className="user-dropdown">
+            <button
+                id={toggleId}
+                aria-haspopup={true}
+                aria-owns={dropdownId}
+                aria-controls={dropdownId}
+                className="user-dropdown__toggle"
+                onClick={() => setOpen(!open)}
+            >
+                <img
+                    alt="Profile and settings menu"
+                    className="user-dropdown__image"
+                    src={placeholder}
+                />
+            </button>
+            <div
+                id={dropdownId}
+                aria-expanded={open}
+                aria-labelledby={toggleId}
+                className={classnames(
+                    'user-dropdown__dropdown',
+                    { 'user-dropdown__dropdown--closed': open },
+                )}
+            >
+                <span className="user-dropdown__dropdown-item">Logged in as {user.user.email}</span>
+                <hr />
+                <ul>
+                    <li className="user-dropdown__dropdown-item">
+                        <a className="user-dropdown__dropdown-item-link" href="/settings">Settings</a>
+                    </li>
+                    <li className="user-dropdown__dropdown-item">
+                        <a className="user-dropdown__dropdown-item-link" href="/help">Help</a>
+                    </li>
+                    <li className="user-dropdown__dropdown-item">
+                        <a className="user-dropdown__dropdown-item-link" href="/logout">Sign out</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
